e8a6cfaaa00afaf90b15581a5eb90dae081014c4
NataliAlena <natali17930@gmail.com>
2021-04-02 14:50:32 +0300

    update new one

--- text.py
+++ text.py
@@ -1,11 +1,11 @@
+file = open ("text.txt" ,"r")
+Word = file.readline().rstrip()
+read = file.read().split()
+file.close()
 def revWord (string):
     wordreverse = string[::-1]
     return wordreverse.lower()
 def countWord(file,Word):
-    file = open ("text.txt" ,"r")
-    Word = file.readline().rstrip()
-    read = file.read().split()
-    file.close()
     cword = 1
     for line in read:
         words = line.split()
