1d106d751ca18834063907d38a799f62c8a59a5b
NataliAlena <natali17930@gmail.com>
2021-04-02 13:13:23 +0300

    update new one

---
+++ text.py
@@ -0,0 +1,17 @@
+def revWord (string):
+    wordreverse = string[::-1]
+    return wordreverse.lower()
+def countWord(file,Word):
+    file = open ("text.txt" ,"r")
+    Word = file.readline().rstrip()
+    read = file.read().split()
+    file.close()
+    cword = 1
+    for line in read:
+        words = line.split()
+        for word in words:
+            revword= revWord(word)
+            if Word==revword:
+                cword+=1
+    return cword
+
